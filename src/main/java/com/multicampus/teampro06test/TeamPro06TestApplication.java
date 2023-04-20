485be47f95eea48e28c5a935e17da4e7901d6b7a
Junhwan <jhjang7408@naver.com>
2023-04-20 13:39:34 +0900

    test1

---
+++ src/main/java/com/multicampus/teampro06test/TeamPro06TestApplication.java
@@ -0,0 +1,13 @@
+package com.multicampus.teampro06test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamPro06TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamPro06TestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/multicampus/teampro06test/TeamPro06TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multicampus.teampro06test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamPro06TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
